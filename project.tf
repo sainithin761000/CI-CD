5704afe4bc250612e7b27e0a215cf36ad2aaedfe
nithin-learning <sainithinmaipath7610@gmail.com>
2024-10-13 18:50:49 -0500

    All the files are added to the repository to test CI/CD.

---
+++ project.tf
@@ -0,0 +1,7 @@
+resource "google_project" "data_project" {
+  billing_account = var.billing_id
+  #folder_id       = var.folder_id
+  #org_id          = var.org_id
+  name            = var.project_id
+  project_id      = var.project_id
+}
---
+++ variables.tf
@@ -0,0 +1,30 @@
+locals {
+  project_id            = "foodlytics76"
+  billing_id            = "017614-73326A-5D8202"
+  region                = "asia-southeast1"
+  zone                  = "asia-southeast1-a"
+  airbyte_machine_type  = "e2-medium"
+  metabase_machine_type = "e2-medium"
+  airflow_machine_type  = "e2-medium"
+  source_datasets = {
+    # To add additional dataset, add values below in the format
+    # dataset_name = "Dataset descriptions"
+    postgres_crm = "Raw data of our company from a Postgres database"
+  }
+}
+
+variable "project_id" {
+  type = string
+}
+
+variable "billing_id" {
+  type = string
+}
+
+variable "folder_id" {
+  type = string
+}
+
+variable "org_id" {
+  type = string
+}
